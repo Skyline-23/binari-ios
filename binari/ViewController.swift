cea2a258b869f21f1accde4fe551c288007de057
apple19760401 <bs2740@naver.com>
2020-10-09 18:29:33 +0900

    Initial Commit

---
+++ binari/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  binari
+//
+//  Created by 김부성 on 10/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
