9a0aba4da091aab7821f88197c5a902fc034ed82
apple19760401 <bs2740@naver.com>
2020-10-31 00:17:37 +0900

    메인화면VC 스토리보드 조금 구현

--- binari/Controllers/mainVC.swift
+++ binari/Controllers/mainVC.swift
@@ -10,11 +10,21 @@ import UIKit
 class mainVC: UIViewController {
     @IBOutlet weak var profile: UIImageView!
     @IBOutlet weak var pinkbg: UIImageView!
+    @IBOutlet weak var banner: UIImageView!
+    //개 이름은 12자까지만 설정할수 있도록 할것
     
     let bgcolor = UIColor(red: 249/255, green: 249/255, blue: 249/255, alpha: 1.0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        banner.layer.shadowColor = UIColor.black.cgColor // 색깔
+        banner.layer.masksToBounds = false  // 내부에 속한 요소들이 UIView 밖을 벗어날 때, 잘라낼 것인지. 그림자는 밖에 그려지는 것이므로 false 로 설정
+        banner.layer.shadowOffset = CGSize(width: 0, height: 4) // 위치조정
+        banner.layer.shadowRadius = 5 // 반경
+        banner.layer.shadowOpacity = 0.3 // alpha값
+        banner.layer.shouldRasterize = true
+        
+        
         
         navigationController?.isNavigationBarHidden = false
         set_profile()
